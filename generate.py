9e086f621e8f97a0f6d76bf0d6f42260083704ec
gnkm <genki.matsunaga@gmail.com>
2022-07-28 06:20:33 +0900

    Add `generate.py`

---
+++ generate.py
@@ -0,0 +1,19 @@
+"""コンテストごとのディレクトリ、ファイルを作成する。
+
+Example:
+    python generate.py abc261 --date 2022-07-23
+
+TODO: 実装のこと
+"""
+
+import sys
+
+import icecream
+
+
+def main():
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
