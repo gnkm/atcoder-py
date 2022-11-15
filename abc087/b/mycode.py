116ecef34d43e9c89c8c5c4723e8cbab378dbbfb
gnkm <genki.matsunaga@gmail.com>
2022-11-15 11:26:46 +0900

    ABC087-B: Change mycode

--- abc087/b/mycode.py
+++ abc087/b/mycode.py
@@ -1,4 +1,6 @@
-"""
+"""ABC087B - Coins
+[AtCoder Beginners Selection]
+
 あなたは、500 円玉を A 枚、100 円玉を B 枚、50 円玉を C 枚持っています。 これらの硬貨の中から何枚かを選び、合計金額をちょうど X 円にする方法は何通りありますか。
 同じ種類の硬貨どうしは区別できません。2 通りの硬貨の選び方は、ある種類の硬貨についてその硬貨を選ぶ枚数が異なるとき区別されます。
 
@@ -14,59 +16,55 @@ X
 https://atcoder.jp/contests/abs/tasks/abc087_b
 """
 
+# for debugging
+import pkg_resources
 
-import copy
+if any([str(i).startswith('icecream') for i in pkg_resources.working_set]):
+    import icecream
+    debug = icecream.ic
+else:
+    debug = print
+
+import pprint
+
+# Scripts starts here.
+from itertools import product
+from operator import mul
 import sys
-from time import sleep
 
-import icecream
+if 'PyPy' in sys.version:
+    import pypyjit
+    pypyjit.set_param('max_unroll_recursion=-1')
+
+mapl = lambda function, iterable, *args: list(map(function, iterable, *args))
+
+input = lambda: sys.stdin.readline().rstrip('\r\n').split()
+i_sesli = lambda: int(input()[0])                  # input single element single line int
+
+sys.setrecursionlimit(1000000)
 
 
-COINS = [500, 100, 50]
+COINS = (500, 100, 50)
 
 
 def main():
-    inputs = [int(input()) for i in range(len(COINS) + 1)]
-    nums = inputs[:3]
-    sum_cost = inputs[-1]
-    coin_num_dict = dict(zip(COINS, nums))
+    num_500 = i_sesli()
+    num_100 = i_sesli()
+    num_050 = i_sesli()
+    target_sum_cost = i_sesli()
+    candidates = product(
+        [i for i in range(num_500 + 1)],
+        [i for i in range(num_100 + 1)],
+        [i for i in range(num_050 + 1)],
+    )
+    ans = 0
+    for c in candidates:
+        sum_cost = sum(map(mul, COINS, c))
+        if sum_cost == target_sum_cost:
+            ans += 1
 
-    print(count_patterns(coin_num_dict, sum_cost))
-
-
-def count_patterns(coin_num_dict, sum_cost):
-    """Return pattern number.
-
-    Args:
-        coin_num_dict (Dict[int, int]): {coin: num, ...}
-        sum_cost (int): [description]
-
-    Returns:
-        int: pattern num
-    """
-    coin_nums = list(coin_num_dict.values())
-
-    if sum_cost == 0:
-        return 1
-
-    if sum(coin_nums) == 0:
-        return 0
-
-    if sum_cost < 0:
-        return 0
-
-    if sum([coin * num for coin, num in coin_num_dict.items()]) < sum_cost:
-        return 0
-
-    pattern_num = 0
-    for coin, num in coin_num_dict.items():
-        for used_num in range(num):
-            _coin_num_dict = copy.deepcopy(coin_num_dict)
-            _coin_num_dict[coin] = used_num
-            pattern_num += count_patterns(_coin_num_dict, sum_cost - coin * used_num,)
-
-    return pattern_num
-
+    print(ans)
+    sys.exit()
 
 if __name__ == '__main__':
     main()
