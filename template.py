1ddd9db640d4dcb4f65a8d59a340201fe3ae67c7
gnkm <genki.matsunaga@gmail.com>
2022-11-15 12:57:06 +0900

    Change template file

--- template.py
+++ template.py
@@ -1,4 +1,20 @@
-import icecream, pprint  # for debugging
+"""Title
+
+Problem statement here.
+"""
+
+# for debugging
+import pkg_resources
+
+if any([str(i).startswith('icecream') for i in pkg_resources.working_set]):
+    import icecream
+    debug = icecream.ic
+else:
+    debug = print
+
+import pprint
+
+# Scripts starts here.
 import sys
 
 if 'PyPy' in sys.version:
@@ -14,14 +30,12 @@ i_sesls = lambda: str(input()[0])                  # input single element single
 i_semli = lambda n: [i_sesli() for _ in range(n)]  # input single element multi line int
 i_semlf = lambda n: [i_seslf() for _ in range(n)]  # input single element multi line float
 i_semls = lambda n: [i_sesls() for _ in range(n)]  # input single element multi line str
-# i_mesli = lambda: list(map(int, input()))          # input multi element single line int
-# i_meslf = lambda: list(map(float, input()))        # input multi element single line float
-i_mesli = lambda: mapl(int, input())          # input multi element single line int
-i_meslf = lambda: mapl(float, input())        # input multi element single line float
+i_mesli = lambda: mapl(int, input())               # input multi element single line int
+i_meslf = lambda: mapl(float, input())             # input multi element single line float
 i_mesls = lambda: list(input())                    # input multi element single line str
-i_memli = lambda n: [i_mesli() for _ in range(n)]    # input multi element multi line int
-i_memlf = lambda n: [i_meslf() for _ in range(n)]    # input multi element multi line float
-i_memls = lambda n: [i_mesls() for _ in range(n)]    # input multi element multi line str
+i_memli = lambda n: [i_mesli() for _ in range(n)]  # input multi element multi line int
+i_memlf = lambda n: [i_meslf() for _ in range(n)]  # input multi element multi line float
+i_memls = lambda n: [i_mesls() for _ in range(n)]  # input multi element multi line str
 
 sys.setrecursionlimit(1000000)
 
