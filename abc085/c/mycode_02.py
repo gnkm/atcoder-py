46612e6b7644a64bf5ca7f7cccf2cd3065fedd2e
gnkm <genki.matsunaga@gmail.com>
2022-12-10 20:40:53 +0900

    ABC085-C: Add my new code

---
+++ abc085/c/mycode_02.py
@@ -0,0 +1,53 @@
+"""ABC085C - Otoshidama
+
+日本でよく使われる紙幣は、10000 円札、5000 円札、1000 円札です。以下、「お札」とはこれらのみを指します。
+
+青橋くんが言うには、彼が祖父から受け取ったお年玉袋にはお札が N 枚入っていて、合計で Y 円だったそうですが、嘘かもしれません。このような状況がありうるか判定し、ありうる場合はお年玉袋の中身の候補を一つ見つけてください。なお、彼の祖父は十分裕福であり、お年玉袋は十分大きかったものとします。
+
+Memo
+
+公式解説で計算量が O(N) となる方法として紹介されていたので実装した。
+https://www.youtube.com/watch?v=-QHM9Dk9hRw&t=2850s
+
+286 ms
+80464 KB
+"""
+
+# for debugging
+import icecream
+debug = icecream.ic
+import pprint
+
+# Scripts starts here.
+import sys
+
+
+input = lambda: sys.stdin.readline().rstrip('\r\n').split()
+
+
+def main():
+    N, Y = map(int, input())
+    Y_d1000 = Y // 1000
+    z_max = min(N, Y_d1000)
+    for z_candidate in range(z_max + 1):
+        if (z_candidate - Y_d1000) % 5 != 0:
+            continue
+        if 5 * N - Y_d1000 > 4 * z_candidate:
+            continue
+        if 9 * z_candidate > 10 * N - Y_d1000:
+            continue
+
+        z = z_candidate
+        for x_candidate in range(N - z + 1):
+            if 10 * x_candidate + 5 * (N - z - x_candidate) == Y_d1000 - z:
+                x = x_candidate
+                y = N - x - z
+                print(f'{x} {y} {z}')
+                sys.exit()
+
+    print('-1 -1 -1')
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
